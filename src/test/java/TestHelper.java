80cfbdaff8b052bb1e17a14bba79ee46d46a3e75
myalcin <yalcinmustafa014@gmail.com>
2023-10-09 22:58:31 +0300

    third

--- src/test/java/TestHelper.java
+++ src/test/java/TestHelper.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.example.Helper.square;
@@ -5,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestHelper {
     @Test
+    @DisplayName("2 ^ 2 = 4")
     void testSquareOfTwo() {
         assertEquals(4, square(2));
     }
